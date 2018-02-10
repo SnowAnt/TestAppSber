7bf645e6269ef5bc5d78d12e04fbb5ece7428b63
Anton Pepelyaev <pepelanton@mail.ru>
2018-02-10 23:41:43 +0300

    Parse data

--- app/src/main/java/pepelyaev/sberbank/com/testappsber/MainActivity.java
+++ app/src/main/java/pepelyaev/sberbank/com/testappsber/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import Data.GPSTracker;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
